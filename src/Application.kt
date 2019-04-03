d20da1abdfe6cea52fb6f4bcccbe7643c3ddfb7e
Marcel Bagemihl <marcel.bagemihl@novatec-gmbh.de>
2019-04-03 13:47:33 +0200

    add delete endpoint and tests for application

--- src/Application.kt
+++ src/Application.kt
@@ -8,16 +8,17 @@ import io.ktor.http.*
 import com.fasterxml.jackson.databind.*
 import io.ktor.jackson.*
 import io.ktor.features.*
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
 import io.ktor.util.pipeline.PipelineContext
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.collections.HashSet
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-    val userlist: ArrayList<User> = ArrayList()
+    val userSet: HashSet<User> = HashSet()
     install(ContentNegotiation) {
         jackson {
             enable(SerializationFeature.INDENT_OUTPUT)
@@ -25,23 +26,41 @@ fun Application.module(testing: Boolean = false) {
     }
     install(CORS) {
         anyHost() // #yolo
+        method(HttpMethod.Delete)
     }
 
     routing {
         post("/users") {
-            val userObject = call.receive<User>()
-            userlist += userObject
-            call.respond(userlist)
+            val requestUser = call.receive<RequestUser>()
+            val userObject = User(UUID.randomUUID(),requestUser.name,requestUser.gender,requestUser.adult,requestUser.avatar)
+
+            userSet.add(userObject)
+
+            call.respond(userSet)
         }
 
         get ("/users") {
-            call.respond(userlist)
+            call.respond(userSet)
         }
 
+        delete("/users/{id}") {
+            val userId = call.parameters["id"]
+            val user = userSet.find { it.id.toString() == userId }
+            userSet.drop(userSet.indexOf(user))
+            call.respond(userSet)
+        }
     }
 }
 
 data class User(
+    val id: UUID,
+    val name: String,
+    val gender: Gender,
+    val adult: Boolean,
+    val avatar: Avatar
+)
+
+data class RequestUser(
     val name: String,
     val gender: Gender,
     val adult: Boolean,
--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -1,24 +1,58 @@
 package de.novatec
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.routing.*
 import io.ktor.http.*
 import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import io.ktor.jackson.*
 import io.ktor.features.*
 import kotlin.test.*
 import io.ktor.server.testing.*
+import java.util.*
 
 class ApplicationTest {
     @Test
-    fun testRoot() {
+    fun testGetEmptyUserList() {
         withTestApplication({ module(testing = true) }) {
-            handleRequest(HttpMethod.Get, "/").apply {
+            handleRequest(HttpMethod.Get, "/users").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("HELLO WORLD!", response.content)
+                assertEquals("[ ]", response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testPostValidUser() {
+        withTestApplication( { module(testing=true) }) {
+            handleRequest(HttpMethod.Post, "/users") {
+                addHeader("content-type", ContentType.Application.Json.toString())
+                setBody("{\n" +
+                        "        \"name\": \"Super Mario\",\n" +
+                        "        \"gender\": \"MALE\",\n" +
+                        "        \"adult\": true,\n" +
+                        "        \"avatar\": \"MARIO\"\n" +
+                        "    }")
+            }
+
+            handleRequest (HttpMethod.Get, "/users").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                val userList:List<User> = jacksonObjectMapper().readValue<ArrayList<User>>(response.content!!)
+                assertEquals(userList.size,1)
             }
         }
     }
 }
+
+data class User(
+    val id: UUID,
+    val name: String,
+    val gender: Gender,
+    val adult: Boolean,
+    val avatar: Avatar
+)
